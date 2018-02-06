0c2993e6bef510af4b8a078c74d1e2dd23c40a7e
Robert Lowe <robert.lowe@maryvillecollege.edu>
2018-02-06 15:21:42 -0500

    Added a demonstration of the sizeof operator

---
+++ pointers/sizeof-demo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "char: " << sizeof(char) << endl
+       << "bool: " << sizeof(bool) << endl
+       << "int: " << sizeof(int) << endl
+       << "float: " << sizeof(float) << endl
+       << "double: " << sizeof(double) << endl;
+}
