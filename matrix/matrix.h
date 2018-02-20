efa9b01969ff919bfdda122cd4fa5db809e62e8c
Robert Lowe <robert.lowe@maryvillecollege.edu>
2018-02-20 15:45:31 -0500

    Fixed a const error

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -14,19 +14,19 @@ public:
     Matrix(int m, int n);
 
     //get the matrix dimensions
-    std::pair<int, int> dim() const;
+    virtual std::pair<int, int> dim() const;
 
     //operators
-    std::vector<double> & operator[](int index);   //returns a row vector reference
-    Matrix operator+(const Matrix &rhs) const;
-    Matrix operator*(double s) const;   //scalar multiplication A * s
+    virtual std::vector<double> & operator[](int index);   //returns a row vector reference
+    virtual Matrix operator+(const Matrix &rhs) const;
+    virtual Matrix operator*(double s) const;   //scalar multiplication A * s
 private:
-    std::vector<std::vector<double>> elements;  //matrix elements
+    virtual std::vector<std::vector<double>> elements;  //matrix elements
 };
 
 //external operators
 double operator*(double s, const Matrix &m);  //scalar multiplcation s * m
 std::ostream & operator<<(std::ostream &os, const Matrix &m);   //extraction operator
-std::istream & operator>>(std::istream &is, const Matrix &m);    //insertion operator
+std::istream & operator>>(std::istream &is, Matrix &m);    //insertion operator
 #endif
 
