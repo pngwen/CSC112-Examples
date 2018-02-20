3b9de8fef6351d093ea5184b7c4b9e378d65f48a
Robert Lowe <robert.lowe@maryvillecollege.edu>
2018-02-20 15:24:05 -0500

    Added matrix.h

---
+++ matrix/matrix.h
@@ -0,0 +1,32 @@
+// A matrix algebra class
+#ifndef MATRIX_H
+#define MATRIX_H
+#include <vector>
+#include <utility>
+#include <iostream>
+
+class Matrix
+{
+public:
+    //Constructors
+    Matrix();
+    Matrix(const Matrix &other);
+    Matrix(int m, int n);
+
+    //get the matrix dimensions
+    std::pair<int, int> dim() const;
+
+    //operators
+    std::vector<double> & operator[](int index);   //returns a row vector reference
+    Matrix operator+(const Matrix &rhs) const;
+    Matrix operator*(double s) const;   //scalar multiplication A * s
+private:
+    std::vector<std::vector<double>> elements;  //matrix elements
+};
+
+//external operators
+double operator*(double s, const Matrix &m);  //scalar multiplcation s * m
+std::ostream & operator<<(std::ostream &os, const Matrix &m);   //extraction operator
+std::istream & istream>>(std::istream &is, const Matrix &m);    //insertion operator
+#endif
+
